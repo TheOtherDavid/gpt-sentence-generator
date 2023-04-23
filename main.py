e24b7d612eb8f0c5a1ef7accc82043d16c51854d
David <davidhand1988@gmail.com>
2023-04-22 21:27:19 -0400

    Add configurable loop time

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from argparse import ArgumentParser
 
 def main():
     args = parse_arguments()
+    config = load_config()
+    loop_time = config["loop_time"]
 
     if args.loop:
         while True:
             print("Executing Hourly GPT Phrase Generator")
             display_gpt_sentence()
             print("Sleeping for an hour...")
-            time.sleep(3600)
+            time.sleep(loop_time)
     else:
         print("Executing Single GPT Phrase Generator")
         display_gpt_sentence()
@@ -27,6 +29,11 @@ def parse_arguments():
         "-l", "--loop", action="store_true", help="Run the program in a loop with an hourly sleep"
     )
     return parser.parse_args()
+
+def load_config():
+    with open("config.json") as f:
+        config = json.load(f)
+    return config
 
 if __name__ == "__main__":
     main()
